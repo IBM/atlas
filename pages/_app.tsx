3690e209a1f7522d06b42d1370ff8e14323809ed
Vince Picone <vp@vincepic.one>
2022-10-27 14:29:27 -0500

    fix: firefox polyfill for has
    Signed-off-by: Vince Picone <vp@vincepic.one>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Header } from "../components/Header";
 import "styles/carbon.scss";
 import "../styles/globals.css";
@@ -5,6 +6,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    async function loadHas() {
+      const cssHasPseudo = (await import("css-has-pseudo/browser")).default;
+      console.log("seu loaded");
+      cssHasPseudo(document, { hover: true, debug: true });
+    }
+    loadHas();
+  });
   return (
     <>
       <Header />
