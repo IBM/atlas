92ede81718048ab4c6e563d0830b2d232addf6ff
Vince Picone <vp@vincepic.one>
2022-12-16 12:17:56 -0500

    export

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "/Vincent-Patrick-Picone/ibm-technology-atlas/"
+      : "",
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
