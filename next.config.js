39b8d0bf49e0a365fc656b6b110193fd5ba586c1
Vince Picone <vp@vincepic.one>
2022-12-16 12:44:13 -0500

    link path

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/Vincent-Patrick-Picone/ibm-technology-atlas",
   assetPrefix:
     process.env.NODE_ENV === "production"
       ? "/Vincent-Patrick-Picone/ibm-technology-atlas/"
