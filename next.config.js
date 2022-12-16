ae1e847e080cbb722ce8318264b2b921af1d64aa
Vince Picone <vp@vincepic.one>
2022-12-16 12:37:39 -0500

    prefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   assetPrefix:
-    process.env.NODE_ENV === "production" ? "/ibm-technology-atlas/" : "",
+    process.env.NODE_ENV === "production"
+      ? "/Vincent-Patrick-Picone/ibm-technology-atlas/"
+      : "",
   reactStrictMode: true,
   swcMinify: true,
 };
