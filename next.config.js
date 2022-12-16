bdfdbf110f297bf165d6003e03104480cd62a9ff
Vince Picone <vp@vincepic.one>
2022-12-16 12:26:21 -0500

    chore: remove prefix?

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "/ibm-technology-atlas/" : "",
   reactStrictMode: true,
   swcMinify: true,
 };
